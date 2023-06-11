3f8766220121609b706698bfb54953a64336f2fc
zhangwei-kumo <kanseefoil@gmail.com>
2023-06-11 20:02:40 +0800

    fixed Chinses problems

--- platform/agent_backend/web/api/agent/tools/reason.py
+++ platform/agent_backend/web/api/agent/tools/reason.py
@@ -5,8 +5,14 @@ from langchain import LLMChain
 from agent_backend.schemas import ModelSettings
 from agent_backend.web.api.agent.model_settings import create_model
 from agent_backend.web.api.agent.tools.tool import Tool
+from langchain import PromptTemplate
 
-
+translator_prompt = PromptTemplate(
+    template="""
+     translate {text} to English.
+    """,
+    input_variables=["text"],
+)
 class Reason(Tool):
     description = (
         "通过现有的信息或理解来推理任务。 "
@@ -23,12 +29,8 @@ class Reason(Tool):
 
         llm = create_model(self.model_settings, streaming=True)
         chain = LLMChain(llm=llm, prompt=execute_task_prompt)
-        print("Reason tool called.")
-        return StreamingResponse.from_chain(
-            chain,
-            {"goal": goal, 
-             "language": "English", 
-             "task": task
-            },
-            media_type="text/event-stream",
-        )
+        trans_chain = LLMChain(llm=llm, prompt=translator_prompt)
+        goal = trans_chain.run({"text": goal})
+        task = trans_chain.run({"text": task})
+        markdown = chain.run( {"goal": goal, "language": "Chinese", "task": task})
+        return  markdown
